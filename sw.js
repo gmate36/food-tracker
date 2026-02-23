a7e976f2d8a479cf1461d79bd9363fb39a7d5bd5
Andrey Barbashin <gmate@mail.ru>
2026-02-23 10:45:04 +0400

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'nutritrack-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE_NAME = 'nutritrack-v2';
+const ASSETS = ['./index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
